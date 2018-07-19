f1c5117be9ee92f5614d9ec2132c35c073bfae68
Susan Vanderplas <srvanderplas+github@gmail.com>
2018-07-19 10:21:18 -0500

    Fix images code

--- Code/Images.R
+++ Code/Images.R
@@ -1,6 +1,5 @@
 library(sf)
-library(raster)
-library(imager)
+# library(imager)
 library(tidyverse)
 
 # Make empty image
@@ -24,27 +23,90 @@ library(tidyverse)
 #   geom_tile(aes(x = x, y = y, fill = as.character(value)), color = "black", data = blankimg_df) + 
 #   geom_sf(aes(fill = "Polygon"), data = st_polygon(list(polycoords)))
 
-img <- load.image(str_replace(df$file[1], "Annotations", "Images") %>% str_replace("xml", "jpg"))
+# img <- imager::load.image(
+#   stringr::str_replace(df$file[1], "Annotations", "Images") %>% 
+#     stringr::str_replace("xml", "jpg"))
+# 
+# poly <- df$annotation[[1]]$poly_sf[[1]]
 
-poly <- df$annotation[[1]]$poly_sf[[1]]
-
-mask_polygon <- function(img, poly, invert = F) {
+mask_polygon <- function(img, poly, invert = F, ret = "image", plotres = T) {
+  stopifnot(ret %in% c("image", "mask"))
   
-  mask <- as.data.frame(img)
+  mask <- as.data.frame(imager::as.cimg(img))
   # flipdf <- mirror(img, "y") %>% as.data.frame()
   
-  polycoords <- as.matrix(poly) %>%
-    as.data.frame() %>% 
-    set_names(c("x", "y"))
-  
-  if (invert) {
-    mask$value <- point.in.polygon(mask$x, mask$y, polycoords$x, polycoords$y)
-  } else {
-    mask$value <- !point.in.polygon(mask$x, mask$y, polycoords$x, polycoords$y)
+  polymat <- as.matrix(poly)
+  if (max(dim(polymat)) == 1) {
+    polymat <- as(polymat[[1]], "matrix")
   }
   
-  mask <- as.cimg(mask, dims = dim(img)) %>% threshold()
+  polycoords <- polymat %>%
+    as.data.frame() %>% 
+    magrittr::set_names(c("x", "y"))
+  
+  if (invert) {
+    mask$value <- sp::point.in.polygon(mask$x, mask$y, polycoords$x, polycoords$y)
+  } else {
+    mask$value <- !sp::point.in.polygon(mask$x, mask$y, polycoords$x, polycoords$y)
+  }
+  
+  mask <- imager::as.cimg(mask, dims = dim(img)) %>% imager::threshold(.1)
   tmp <- img
   tmp[mask == 0] <- 1
-  plot(tmp)
+  
+  if (ret == "image") {
+    if (plotres) {
+      plot(tmp)
+    }
+    return(tmp)
+  } 
+  
+  if (ret == "mask") {    
+    if (plotres) {
+      plot(mask)
+    }
+    return(mask)
+  }
 }
+
+fix_img <- function(im, poly, invert, ret, angle) {
+  if (is.character(im)) im <- imager::load.image(im)
+  
+  try({
+    x <- mask_polygon(img = im, poly = poly, ret = ret, invert = invert, plotres = F) %>%
+      imager::imrotate(angle = angle, boundary = 1) %>%
+      imager::autocrop()
+    
+    # plot(x)
+    x
+  })
+}
+
+fix_img_all <- function(impath, imagedf, ret = "image", invert = T, angle = 0) {
+  im <- imager::load.image(impath)
+  
+  fix_img_sub <- partial(fix_img, im = im)
+  select(imagedf, poly, angle) %>%
+    mutate(ret = ret, invert = invert) %>%
+    pmap(fix_img_sub)
+}
+
+img_split_resize <- function(im, xsize = 256, ysize = 256, interpolation_type = 1, boundary_conditions = 0) {
+  splitimgs <- imager::imsplit(im, axis = "x", nb = -xsize) %>%
+    imager::as.imlist() %>%
+    map(imager::imsplit, axis = "y", nb = -ysize) %>%
+    unlist(recursive = F) %>%
+    imager::as.imlist()
+  
+  resizeimgs <- splitimgs %>%
+    map(imager::resize, size_x = xsize, size_y = ysize, 
+        interpolation_type = interpolation_type, 
+        boundary_conditions = boundary_conditions) %>%
+    imager::as.imlist()
+  
+  enough_to_resize_idx <- sapply(splitimgs, function(x) prod(dim(x)[1:2] >= c(xsize/10, ysize/10)) == 1)
+  color_variation_idx <- sapply(resizeimgs, function(x) length(unique(x)) >= 50)
+  
+  resizeimgs[enough_to_resize_idx & color_variation_idx]
+}
+
