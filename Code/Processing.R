cc0acee0053253f53926bbd1ab9267ef1321ab74
Susan Vanderplas <srvanderplas+github@gmail.com>
2018-09-21 09:07:49 -0500

    Clean up and change to use flexible directories (e.g. change from parent to child script)

--- Code/Processing.R
+++ Code/Processing.R
@@ -9,23 +9,38 @@ library(tidyverse)
 library(furrr)
 plan(multicore, workers = 40)
 
-source("Code/Images.R")
-source("Code/ParseAnnotations.R")
+################################################################################
+### Set up directories if not already in the environment (e.g. if this is    ###
+### not called from another script)                                          ###
+################################################################################
+if (!exists("code_dir")) {
+  code_dir <- "Code"
+}
 
-# files <- list.files("Annotations/Shoes", "*.xml", full.names = T) #%>% sample(size = 150)
-# images <- str_replace(files, "Annotations", "Images") %>% str_replace("xml", "jpg")
-# file.copy(files, file.path("Test", "Annotations", basename(files)))
-# file.copy(images, file.path("Test", "Images", basename(images)))
+if (!exists("image_dir")) {
+  image_dir <- "Images/Shoes"
+}
 
-files <- list.files("Annotations/Shoes", "*.xml", full.names = T)
-images <- str_replace(files, "Annotations", "Images") %>% str_replace("xml", "jpg")
+if (!exists("annotation_dir")) {
+  annotation_dir <- "Annotations/Shoes"
+}
+
+if (!exists("process_dir")) {
+  process_dir <- file.path(getwd(), "OneHotContext")
+}
+################################################################################
+
+source(file.path(code_dir, "Images.R"))
+source(file.path(code_dir, "ParseAnnotations.R"))
+
+files <- list.files(annotation_dir, "*.xml", full.names = T)
+images <- file.path(image_dir, str_replace(basename(files), "xml", "jpg"))
 
 df <- data_frame(
   id = 1:length(files),
   file = files,
   image = images
 ) %>%
-  # filter(row_number() < 500) %>%
   mutate(
     xml = map(file, annotationObj_extract),
     objs = map_int(xml, ~length(unlist(.)))
@@ -34,25 +49,29 @@ df <- data_frame(
   mutate(annot = future_map(xml, annotationObj_parse_rename))
 
 # Reset future processes
-# plan(sequential)
-# gc()
-# plan(multiprocess, workers = 40)
+plan(sequential)
+gc()
+plan(multicore, workers = 40)
 
-# dfunnest <- df %>% select(-xml) %>% unnest()
 
 df <- df %>%
   mutate(
     fullannot = future_map(annot, ~try(polygon_addfulllabels(.)))
-    # intersection = future_map(annot, polygon_intersect),
-    # intersect_segs = future_map_int(intersection, nrow)
   )
 
-which(sapply(df$fullannot, function(x) "try-error" %in% class(x)))
+sapply(df$fullannot, function(x) "try-error" %in% class(x)) %>%
+  which() %>% 
+  paste(collapse = ", ") %>%
+  paste("Errors in annotations", .) %>%
+  message()
 
 df_work <- dplyr::select(df, -xml) # make it easier to see what's going on
 
+if (!dir.exists(file.path(process_dir, "toslice"))) {
+  dir.create(file.path(process_dir, "toslice"))
+}
+
 dfunion <- dplyr::select(df_work, id, image, fullannot) %>%
-  # filter(row_number() < 200) %>%
   unnest() %>%
   filter(!str_detect(name, "exclude")) %>%
   mutate(geost = future_map(poly_sf, geo_stats)) %>%
@@ -60,19 +79,15 @@ dfunion <- dplyr::select(df_work, id, image, fullannot) %>%
   filter(area > 64^2, area / diagdist > 64 * .5) %>%
   ungroup() %>%
   group_by(image, name) %>%
-  select(id, image, name, attributes, objID = id1, poly_sf, area, diagdist, angle, angle_adj, mbr) %>%
+  select(id, image, name, attributes, objID = id1, poly_sf, area, diagdist, 
+         angle, angle_adj, mbr) %>%
   mutate(toobig = ifelse(area > 680^2, "toslice/", ""),
          mbr = sf::st_polygon(mbr)) %>% 
   group_by(image, name) %>%
-  mutate(filename = sprintf("OneHotContext/%s%s-%d-%s", toobig, name, row_number(), basename(image))) %>%
+  mutate(filename = sprintf("%s/%s%s-%d-%s", process_dir, toobig, name, 
+                            row_number(), basename(image))) %>%
   ungroup()
 
-# dfintersect <- dplyr::select(df_work, id, image, intersection) %>%
-#   unnest() %>%
-#   filter(name.1 != "exclude" & name.2 != "exclude" & name.1 != "other" & name.2 != "other") %>%
-#   mutate(geost = map(geometry, geo_stats)) %>%
-#   unnest(geost) %>%
-#   filter(area > 128^2, area/diagdist > 128*.5)
 
 # Create chunks of data frame
 dfsplit <- split(dfunion, floor(dfunion$id / 10))
